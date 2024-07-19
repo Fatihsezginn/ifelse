efd02b2bda013bcc2c1ac5f7cab39ae351aa0672
pc <pc@DESKTOP-78M62IA>
2024-07-19 10:39:25 +0300

    Add project files.

---
+++ ifelse/Program.cs
@@ -0,0 +1,21 @@
+﻿
+            Console.Write("Lutfen bir sayi giriniz.");
+            int sayi = Convert.ToInt32 (Console.ReadLine());
+            if(sayi == 10)
+            {
+                Console.Write("Girilen sayi 10'a eşittir."); 
+            }
+            else if(sayi > 10)
+            {
+                Console.Write("Girilen sayı 10'dan büyüktür.");
+            }
+            else
+            {
+                Console.Write("Girilen sayi 10'dan küçüktür.");
+            }
+            if (sayi % 2 == 0)
+            {
+                Console.Write("Girilen sayi çifttir");
+            }
+            else
+                Console.Write("Girilen sayi tektir.");
